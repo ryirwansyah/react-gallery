35a51afa502dfcfcbff6d55a86f704a7e354cec8
ryirwansyah <rynlda69@gmail.com>
2018-08-06 22:35:36 +0700

    Solving Gallery Bug

--- src/GalleryMap.jsx
+++ src/GalleryMap.jsx
@@ -3,24 +3,112 @@ import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 
 const photos = [
-  { src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799', width: 1, height: 1 },
-  { src: 'https://source.unsplash.com/qDkso9nvCg0/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799', width: 1, height: 1 },
-  { src: 'https://source.unsplash.com/qDkso9nvCg0/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
+  {
+    src: '/media/Peta Kontur.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Kontur"
+  }, {
+    src: '/media/PETA JARINGAN JALAN.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Jaringan Jalan"
+  }, {
+    src: '/media/PETA JARINGAN JALAN-1.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Jaringan Jalan-1"
+  }, {
+    src: '/media/I-1 (PETA 1). peta administrasi.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Administrasi"
+  }, {
+    src: '/media/III-1 (PETA 2). peta sistem perkotaan.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Sistem Perkotaan"
+  }, {
+    src: '/media/III-2 (PETA 3). peta pengembangan permukiman.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Pengembangan Permukiman"
+  }, {
+    src: '/media/III-3 (PETA 4). peta pengembangan jl bebas hambatan.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Pengembangan Jalan Bebas Hambatan"
+  }, {
+    src: '/media/III-4 (PETA 5). peta jaringan transportasii.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Jaringan transportasi"
+  }, {
+    src: '/media/III-6 (PETA 6). peta rencana struktur ruang.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Rencana Struktur Ruang"
+  }, {
+    src: '/media/IV-1.a (PETA 7). peta DAS.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta DAS"
+  }, {
+    src: '/media/IV-1.b (PETA 8). peta perlindungan kawasan setempat.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Perlindungan Kawasan Setempat"
+  }, {
+    src: '/media/IV-2 (PETA 9).peta kawasan lindung.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Kawasan Lindung"
+  }, {
+    src: '/media/IV-3.(PETA 10) peta rawan bencana.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Rawan Bencana"
+  }, {
+    src: '/media/IV-4.   (PETA 11) skema evakuasi.jpg',
+    width: 4,
+    height: 3,
+    caption: "Skema evakuasi"
+  }, {
+    src: '/media/IV-4. (PETA 12) peta sebaran kawasan tambang.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Sebaran Kawasan Tambang"
+  }, {
+    src: '/media/IV-5. (PETA 13) peta geologi.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta geologi"
+  }, {
+    src: '/media/IV-6. (PETA 14) peta sebaran situs wisata.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Sebaran Situs Wisata"
+  }, {
+    src: '/media/IV-7. (PETA 15) peta kawasan hankam.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Kawasan Hankam "
+  }, {
+    src: '/media/IV-8. (PETA 16) peta rencana pola ruang.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Rencana Pola Ruang"
+  }, {
+    src: '/media/V-1. (PETA 17) peta deliniasi KSN & KSK.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Deliniasi LSN & KSK"
+  }, {
+    src: '/media/V-2.(PETA 18) peta kawasan strategis.jpg',
+    width: 4,
+    height: 3,
+    caption: "Peta Kawasan Strategis"
+  }
 ];
 
 
@@ -70,7 +158,7 @@ class GalleryMap extends React.Component {
                 <h1 className="gallery-title">Peta Analisis</h1>
             </div>
           </div>
-          <Gallery photos={photos.slice(0,10)} onClick={this.openLightbox} />
+          <Gallery photos={photos.slice(5,18)} onClick={this.openLightbox} />
         </div>
         <Lightbox images={photos}
           onClose={this.closeLightbox}
